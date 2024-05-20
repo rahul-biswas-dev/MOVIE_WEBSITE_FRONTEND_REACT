d687a5afbe23056fabc4220cc28438b7f1d37d4f
rahulprow <141057669+rahulprow@users.noreply.github.com>
2024-05-20 20:26:48 +0530

    commit 2

--- movieNewDesigbn/src/App.jsx
+++ movieNewDesigbn/src/App.jsx
@@ -1,16 +1,29 @@
-import Home from './components/Home';
-import Navbar from './components/Navbar';
-import './App.css'
+/* eslint-disable no-unused-vars */
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
-
+  const [theme, setTheme] = useState("light");
+  useEffect(() => {
+     if (theme === "light") {
+       document.body.classList.remove("dark");
+       document.body.classList.add("light");
+     } else {
+       document.body.classList.remove("light");
+       document.body.classList.add("dark");
+     }
+   },[theme])
 
   return (
     <>
-      <Home />
-      <Navbar/>
+      <Navbar theme={theme} setTheme={setTheme} />
+      <main className={theme}>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- movieNewDesigbn/src/components/Navbar.jsx
+++ movieNewDesigbn/src/components/Navbar.jsx
@@ -1,22 +1,56 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import search_light from "../assets/NavbarAssets/search-w.png";
 import search_dark from "../assets/NavbarAssets/search-b.png";
 import light from "../assets/NavbarAssets/night.png";
 import dark from "../assets/NavbarAssets/day.png";
-const Navbar = () => {
+
+
+const Navbar = ({ theme, setTheme }) => {
+  
+  const toggle_mode = () => {
+  theme == "light" ? setTheme("dark") : setTheme("light")
+  }
+
   return (
     <>
-      <nav className="w-full h-14 bg-indigo-300 flex justify-between px-3 items-center md: px4">
-        <div className="text-2xl text-indigo-900 font-bold">LOGO</div>
-        <ul className="md:flex hidden  font-semibold">
-          <li>Menu</li>
-          <li>Watch List</li>
-          <li>Sign in</li>
-        </ul>
-        <div>
-          <input type="text" placeholder="Search" />
-          <img src={search_dark} alt="" />
+      {/* nav bar starts here */}
+      <nav className="w-full h-14 bg-indigo-300  flex justify-between items-center p-2 md:pr-20 md:pl-20">
+        <div className="flex items-center mr-1 md:mr-24 ">
+          <div className="text-xs md:text-xl text-indigo-800 font-bold mr-1 md:mr-4 bg-yellow-400 p-1 rounded-md cursor-pointer m-">
+            <a href="#" className="no-underline">
+              MovieHive
+            </a>
+          </div>
+
+          <div className="md:block hidden text-xl text-indigo-800  font-bold mr-auto cursor-pointer">
+            &#8801; Menu
+          </div>
         </div>
-        <img src={light} alt="" />
+
+        <div className="flex items-center mx-auto rounded-xl bg-black flex-grow">
+          <input
+            type="text"
+            placeholder="Search Movies"
+            className=" bg-transparent py-[5px] outline-none md:px-2 md:py-1 flex-grow"
+          />
+
+          <div className="flex w-[20px]">
+            <img src={search_light} alt="search" className="w-3" />
+          </div>
+        </div>
+
+        <div className="flex items-center ml-5 md:ml-24">
+          <div className="hidden md:block px-4 py-1 text-xl text-white font-bold  mr-4 cursor-pointer rounded-md bg-indigo-700">
+            <button>Sign In</button>
+          </div>
+          <div className="w-12 cursor-pointer hidden md:block">
+            <img onClick={()=>{toggle_mode()}} src={theme == "light" ? light : dark} alt="" />
+          </div>
+          <div className="md:hidden flex text-xl">
+            <a href="#">&#8801;</a>
+          </div>
+        </div>
       </nav>
     </>
   );
